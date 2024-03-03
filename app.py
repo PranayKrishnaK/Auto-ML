4e23132b6ee4de7f6e5622879208b65ed040cbae
Pranay Krishna <pranaykrishna1143@gmail.com>
2024-03-03 23:26:18 +0530

    first commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit as st
 import pandas as pd
 #import matplotlib.pyplot as plt
 #import seaborn as sns
-from pycaret.datasets import get_data
+#from pycaret.datasets import get_data
 #from sklearn.metrics import confusion_matrix
 from pycaret.classification import *
 from pycaret.anomaly import *
